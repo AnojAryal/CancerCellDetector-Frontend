6069b5663f680548d9d2ef9353e2231dcc565665
anojaryal <anoj1810@gmail.com>
2024-04-26 18:00:26 +0545

    building color mode switch

--- cancercelldetection-frontened/src/components/NavBar.tsx
+++ cancercelldetection-frontened/src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react"
-import logo from "../assets/logo.jpg";
+import logo from "../assets/logo.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 
 const NavBar = () => {
   return (
-    <HStack> 
+    <HStack justifyContent='space-between' padding='10px'> 
         <Image src={logo} boxSize='50px'/>
         <Text>NavBar</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }
