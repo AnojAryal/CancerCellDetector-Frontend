96295f4706bc67f79ede7badf0140d347197a3a1
anojaryal <anoj1810@gmail.com>
2024-04-29 19:09:38 +0545

    axios setup

--- cancercelldetection-frontened/src/App.tsx
+++ cancercelldetection-frontened/src/App.tsx
@@ -13,11 +13,11 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="red">
+        <GridItem area="aside">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="green">
+      <GridItem area="main">
         Main
       </GridItem>
     </Grid>
