eda593e003e62f67419425ed8c348e38f2f0db8e
anojaryal <anoj1810@gmail.com>
2024-05-06 17:42:21 +0545

    signup completed

--- cancercelldetection-frontened/src/services/api-client.ts
+++ cancercelldetection-frontened/src/services/api-client.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+const baseURL = 'http://127.0.0.1:8000';
 
-export default axios.create ({
-    baseURL : 'http://localhost:3000/'
-})
+const apiClient = axios.create({
+    baseURL: baseURL
+});
+
+export default apiClient;
