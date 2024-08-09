0aeb51cf6823c85ec199c4b30472053aa4078ea0
AnojAryal <anoj1810@gmail.com>
2024-08-09 14:33:25 +0545

    Detect Cncer cell component ui updated

--- src/components/patient/DetectCancerCell.tsx
+++ src/components/patient/DetectCancerCell.tsx
@@ -3,16 +3,14 @@ import {
   Button,
   Heading,
   Input,
-  SimpleGrid,
   Text,
   useColorModeValue,
-  VStack,
   HStack,
-  Icon,
+  Image,
+  Flex,
   Grid,
   GridItem,
 } from "@chakra-ui/react";
-import { BiEditAlt } from "react-icons/bi";
 import { useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -50,130 +48,175 @@ const DetectCancerCell = () => {
     event.preventDefault();
   };
 
+  const handleClear = () => {
+    setSelectedFiles([]);
+  };
+
   return (
     <Box p={8} pt={20} bg={boxBgColor} color={textColor} minH="100vh">
-      <HStack alignItems="center" mb={6}>
-        <Heading as="h1" size="lg">
-          {title}
-        </Heading>
-      </HStack>
-      <Text mb={8} fontSize="md">
-        {description}
-      </Text>
-      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing={8}>
-        <Box>
-          <HStack justifyContent="space-between" alignItems="center" mb={4}>
-            <Heading as="h3" size="md" fontWeight="bold">
-              Test Dataset
+      <Flex direction="column" height="100%">
+        <Box mb={6}>
+          <HStack alignItems="center" mb={6}>
+            <Heading as="h1" size="lg">
+              {title}
             </Heading>
-            <Icon as={BiEditAlt} boxSize={5} cursor="pointer" />
           </HStack>
-          <Box
-            border="1px solid"
-            borderColor={borderColor}
-            borderRadius="md"
-            p={4}
-            boxShadow={shadowColor}
-            height="200px"
-          ></Box>
+          <Text mb={8} fontSize="md">
+            {description}
+          </Text>
         </Box>
-        <Box>
-          <HStack justifyContent="space-between" alignItems="center" mb={4}>
-            <Heading as="h3" size="md" fontWeight="bold">
-              Upload Image
-            </Heading>
-          </HStack>
-          <Box
-            border="2px dashed"
-            borderColor={borderColor}
-            p={4}
-            borderRadius="md"
-            textAlign="center"
-            boxShadow={shadowColor}
-            width="100%"
-            height="100px"
-            onClick={handleClick}
-            onDrop={handleDrop}
-            onDragOver={handleDragOver}
-            cursor="pointer"
-          >
-            <Text mb={2} fontSize="lg">
-              Drag & drop images here, or click to select files
-            </Text>
-            <Input
-              type="file"
-              multiple
-              ref={fileInputRef}
-              display="none"
-              onChange={(e) => handleFiles(e.target.files)}
-            />
+
+        <Flex direction={{ base: "column", md: "row" }} gap={8} flex="1" mb={6}>
+          <Box flex="1" minWidth="0" height="200px">
+            <HStack justifyContent="space-between" alignItems="center" mb={4}>
+              <Heading as="h3" size="md" fontWeight="bold">
+                Test Dataset
+              </Heading>
+            </HStack>
+            <Box
+              border="1px solid"
+              borderColor={borderColor}
+              borderRadius="md"
+              p={4}
+              boxShadow={shadowColor}
+              height="100%"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Text fontSize="lg">Dataset Overview</Text>
+            </Box>
           </Box>
-          <VStack spacing={4} align="stretch" mt={4}>
+          <Box flex="1" minWidth="0" height="200px">
+            <HStack justifyContent="space-between" alignItems="center" mb={4}>
+              <Heading as="h3" size="md" fontWeight="bold">
+                Upload Image
+              </Heading>
+            </HStack>
+            <Box
+              border="2px dashed"
+              borderColor={borderColor}
+              p={4}
+              borderRadius="md"
+              boxShadow={shadowColor}
+              width="100%"
+              height="100%"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              onClick={handleClick}
+              onDrop={handleDrop}
+              onDragOver={handleDragOver}
+              cursor="pointer"
+            >
+              <Text mb={2} fontSize="lg">
+                Drag & drop images here, or click to select files
+              </Text>
+              <Input
+                type="file"
+                multiple
+                ref={fileInputRef}
+                display="none"
+                onChange={(e) => handleFiles(e.target.files)}
+              />
+              {selectedFiles.length > 0 && (
+                <>
+                  <HStack spacing={2} mt={2} wrap="wrap" mb={4}>
+                    {selectedFiles.map((file, index) => (
+                      <Box
+                        key={index}
+                        border="1px solid"
+                        borderColor={borderColor}
+                        borderRadius="md"
+                        p={2}
+                        boxShadow={shadowColor}
+                      >
+                        <Image
+                          src={URL.createObjectURL(file)}
+                          alt={`Selected ${file.name}`}
+                          boxSize="100px"
+                          objectFit="cover"
+                        />
+                      </Box>
+                    ))}
+                  </HStack>
+                </>
+              )}
+            </Box>
             {selectedFiles.length > 0 && (
-              <Box>
-                {selectedFiles.map((file, index) => (
-                  <Text key={index}>{file.name}</Text>
-                ))}
-              </Box>
+              <Flex justifyContent="center" mt={4}>
+                <HStack spacing={4}>
+                  <Button colorScheme="green" size="sm">
+                    Upload
+                  </Button>
+                  <Button colorScheme="red" size="sm" onClick={handleClear}>
+                    Clear
+                  </Button>
+                </HStack>
+              </Flex>
             )}
-            <Button colorScheme="green" size="md" alignSelf="center">
+          </Box>
+          
+        </Flex>
+        <Flex mt={4}>
+          <HStack spacing={4}>
+            <Button colorScheme="green" mt={4} size="md">
               Process Data
             </Button>
-            <Text color="red.500" textAlign="center">
-              WebSocket connection status: disconnected
-            </Text>
-          </VStack>
+          </HStack>
+        </Flex>
+
+        <Box mt={8}>
+          <Heading as="h2" size="lg" mb={6}>
+            Results
+          </Heading>
+          <Grid templateColumns={{ sm: "1fr", md: "repeat(3, 1fr)" }} gap={6}>
+            <GridItem>
+              <Box
+                border="1px solid"
+                borderColor={borderColor}
+                borderRadius="md"
+                p={4}
+                boxShadow={shadowColor}
+                height="200px"
+              >
+                <Text mb={2} fontSize="md" fontWeight="bold">
+                  ID: 2c7ff942-9f47-4dcb-a858-4ce574e08c09
+                </Text>
+              </Box>
+            </GridItem>
+            <GridItem>
+              <Box
+                border="1px solid"
+                borderColor={borderColor}
+                borderRadius="md"
+                p={4}
+                boxShadow={shadowColor}
+                height="200px"
+              >
+                <Text mb={2} fontSize="md" fontWeight="bold">
+                  ID: 903adf83-0500-4494-8055-2ce873d4c550
+                </Text>
+              </Box>
+            </GridItem>
+            <GridItem>
+              <Box
+                border="1px solid"
+                borderColor={borderColor}
+                borderRadius="md"
+                p={4}
+                boxShadow={shadowColor}
+                height="200px"
+              >
+                <Text mb={2} fontSize="md" fontWeight="bold">
+                  ID: 48959aba-ef59-42cf-87a7-5fe1b0089ea1
+                </Text>
+              </Box>
+            </GridItem>
+          </Grid>
         </Box>
-      </SimpleGrid>
-      <Box mt={8}>
-        <Heading as="h2" size="lg" mb={6}>
-          Results
-        </Heading>
-        <Grid templateColumns={{ sm: "1fr", md: "repeat(3, 1fr)" }} gap={6}>
-          <GridItem>
-            <Box
-              border="1px solid"
-              borderColor={borderColor}
-              borderRadius="md"
-              p={4}
-              boxShadow={shadowColor}
-              height="200px"
-            >
-              <Text mb={2} fontSize="md" fontWeight="bold">
-                ID: 2c7ff942-9f47-4dcb-a858-4ce574e08c09
-              </Text>
-            </Box>
-          </GridItem>
-          <GridItem>
-            <Box
-              border="1px solid"
-              borderColor={borderColor}
-              borderRadius="md"
-              p={4}
-              boxShadow={shadowColor}
-              height="200px"
-            >
-              <Text mb={2} fontSize="md" fontWeight="bold">
-                ID: 903adf83-0500-4494-8055-2ce873d4c550
-              </Text>
-            </Box>
-          </GridItem>
-          <GridItem>
-            <Box
-              border="1px solid"
-              borderColor={borderColor}
-              borderRadius="md"
-              p={4}
-              boxShadow={shadowColor}
-              height="200px"
-            >
-              <Text mb={2} fontSize="md" fontWeight="bold">
-                ID: 48959aba-ef59-42cf-87a7-5fe1b0089ea1
-              </Text>
-            </Box>
-          </GridItem>
-        </Grid>
-      </Box>
+      </Flex>
     </Box>
   );
 };
